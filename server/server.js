a37d45c3fb455a4479d06d3d38eff076934aff9a
PO CHANG CHEN <kk741852963tw1610@gmail.com>
2022-06-24 07:11:07 +0800

    new .env

--- server/server.js
+++ server/server.js
@@ -1,5 +1,6 @@
+require("dotenv").config();
 const express = require("express");
-const port = 3000;
+const port = process.env.PORT || 3000;
 const path = require("path");
 const axios = require("axios");
 const app = express();
